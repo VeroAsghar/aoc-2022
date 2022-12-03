6062dd54851ac82d2e6f5e38390f932239f04140
gralvarek <james.asghar@gmail.com>
2022-12-03 14:32:13 +0100

    Finished day3...lifetimes are annoying

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,7 +1,19 @@
 use std::str::FromStr;
 
 fn main() {
-    println!("Hello, world!");
+    const INPUT: &str = include_str!("day3.input");
+    let lines: Vec<String> = INPUT.lines().map(|line| line.to_string()).collect();
+    let nr_of_groups = lines.len() / 3;
+    let mut lines = lines.iter();
+
+    let groups = (0..nr_of_groups).into_iter().map(|_| {
+        Group::parse(&mut lines.by_ref().take(3))
+    }).inspect(|g| {
+        println!("{:?}", g)
+    }).collect::<Vec<Group>>();
+
+    let sum_priorities: usize = groups.into_iter().map(|group| priority(group.find_repeat())).sum();
+    println!("{}", sum_priorities);
 }
 
 struct Rucksack {
@@ -10,14 +22,61 @@ struct Rucksack {
 }
 
 impl Rucksack {
-    pub fn sum_priorities() {
-        todo!();
+
+    fn find_repeat(&self) -> char {
+        let c1: Vec<char> = self.first.chars().collect();
+        let c2: Vec<char> = self.second.chars().collect();
+        for i in &c1 {
+            for j in &c2 {
+                if *i == *j {
+                    return *i
+                }
+            }
+        }
+        panic!();
     }
 
-    fn find_repeat_char(&self) -> usize {
-        self.first.chars().cmp
+}
+
+#[derive(Debug)]
+struct Group {
+    first: String,
+    second: String,
+    third: String,
+}
+
+pub fn priority(c: char) -> usize {
+    let mut letters: Vec<char> = ('a'..='z').into_iter().collect::<Vec<char>>();
+    let uppercase: Vec<char> = ('A'..='Z').into_iter().collect::<Vec<char>>();
+    letters.extend(uppercase);
+    letters.into_iter().position(|x| x == c).unwrap() + 1
+
+}
+impl Group {
+pub fn find_repeat(&self) -> char {
+    let c1: Vec<char> = self.first.chars().collect();
+    let c2: Vec<char> = self.second.chars().collect();
+    let c3: Vec<char> = self.third.chars().collect();
+    for i in &c1 {
+        for j in &c2 {
+            for k in &c3 {
+                if *i == *j && *i == *k {
+                    return *i
+                }
+            }
+        }
+    }
+    panic!();
+}
+    pub fn parse<'a, I: Iterator<Item=&'a String>>(iter: &mut I) -> Self {
+        Group {
+            first: iter.next().unwrap().to_string(),
+            second: iter.next().unwrap().to_string(),
+            third: iter.next().unwrap().to_string(),
+        }
     }
 }
+
 
 impl FromStr for Rucksack {
     type Err = ();
@@ -30,6 +89,7 @@ impl FromStr for Rucksack {
         })
     }
 }
+
 
 #[cfg(test)]
 mod test {
@@ -44,7 +104,24 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
     fn example1() {
-        let rucksacks = INPUT.lines().map(|line| line.parse().unwrap()).collect::<Vec<Rucksack>>();
+        let sum_priorities = INPUT.lines().map(|line| priority(line.parse::<Rucksack>().unwrap().find_repeat())).sum::<usize>();
+        assert_eq!(sum_priorities, 157);
 
+    }
+
+    #[test]
+    fn example2() {
+        let lines: Vec<String> = INPUT.lines().map(|line| line.to_string()).collect();
+        let nr_of_groups = lines.len() / 3;
+        let mut lines = lines.iter();
+
+        let groups = (0..nr_of_groups).into_iter().map(|_| {
+            Group::parse(&mut lines.by_ref().take(3))
+        }).inspect(|g| {
+            println!("{:?}", g)
+        }).collect::<Vec<Group>>();
+
+        let sum_3_priorites: usize = groups.into_iter().map(|group| priority(group.find_repeat())).sum();
+        assert_eq!(sum_3_priorites, 70);
     }
 }
