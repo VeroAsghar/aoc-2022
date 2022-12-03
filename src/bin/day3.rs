5d8044fc9ddcf66bd17b172348f870ac8a486f37
James Asghar <9677449-james.asghar@users.noreply.gitlab.com>
2022-12-03 12:21:39 +0100

    working on day 3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,3 +1,50 @@
+use std::str::FromStr;
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Rucksack {
+    first: String,
+    second: String
+}
+
+impl Rucksack {
+    pub fn sum_priorities() {
+        todo!();
+    }
+
+    fn find_repeat_char(&self) -> usize {
+        self.first.chars().cmp
+    }
+}
+
+impl FromStr for Rucksack {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (s1, s2) = s.split_at(s.len()/2);
+        Ok(Rucksack {
+            first: s1.to_string(),
+            second: s2.to_string(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    #[test]
+    fn example1() {
+        let rucksacks = INPUT.lines().map(|line| line.parse().unwrap()).collect::<Vec<Rucksack>>();
+
+    }
 }
